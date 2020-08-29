61783b1b9c14361ec5aaab11dd28a69b4c779bae
Patricio Echeverria <patricio.echeverria@gmail.com>
2020-08-29 18:11:17 -0400

    Agrega distribucion de frecuenca para cada columna

--- inicial_script.R
+++ inicial_script.R
@@ -6,4 +6,54 @@ dir1 <- paste(getwd(), "/DEVF/Curso Data Science/Proyecto/devF_proyecto/Assets",
 WD <- normalizePath(dir1, winslash = "\\", mustWork = NA)
 f416 <- read.csv(paste(WD, "Abr2019-Jun2019wet.csv", sep="\\"), stringsAsFactors = FALSE)
 
+library('tidyverse')
+library(dplyr) #manejo mas eficaz de dataframe
+
+
 str(f416)
+sapply(f416, function(x) sum(is.na(x)))
+
+
+
+### SECCION DE MANIPULACION DE DATOS ELIMINAR SUMAR BORRAR AGREGAR CAMBIAR ########
+#-----------------------------------------------------------------------------------#
+
+### ELIMINACION DE COLUMNAS
+f416 <- f416 %>%
+  select(-MOP21) %>%
+  select(-MOP31)
+
+### Sumar cantidad de datos de una columna
+f416$Name1=="MANUELMARIN"
+sum(f416$Name1=="MANUELMARIN")
+
+### Modificar los valores eliminando o agregando caracteres
+f416$Name1 = gsub('"', '', f416$Name1)
+f416$Name1 = gsub(' ', '', f416$Name1)
+
+#------------------------------------------------------------------------#
+#------------------------------------------------------------------------#
+
+
+sapply(f416, function(x) sum(is.na(x)))
+
+colnames(f416)
+rowname(f416)
+f416[,2]
+class(f416[,2])
+is.numeric(f416[,2])
+table(f416[,2])
+table(f416[,3])
+table(f416[,3])
+
+
+#### Creamos un bucle que recorrera la dimension completa (n columnas) de nuestra dataframe
+#### imprimiendo el nombre de la columna y una tabal con la distribucion absoluta de c/column
+for(i in 1:dim(f416)[2]) {
+  print(colnames(f416)[i])
+  print(table(f416[,i]))
+}
+
+#### Tratamientos de NA
+#--------------------------------------------------------------------------
+
