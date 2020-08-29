26738395555a1194afb893394363037a2579e501
Patricio Echeverria <patricio.echeverria@gmail.com>
2020-08-28 20:59:25 -0400

    csv repaired inictial_script import line

---
+++ inicial_script.R
@@ -0,0 +1,7 @@
+rm(list=ls())
+
+### Import file csv
+setwd('~')
+dir1 <- paste(getwd(), "/DEVF/Curso Data Science/Proyecto/devF_proyecto/Assets", sep="")
+WD <- normalizePath(dir1, winslash = "\\", mustWork = NA)
+f416 <- read.csv(paste(WD, "Abr2019-Jun2019wet.csv", sep="\\"), stringsAsFactors = FALSE)
