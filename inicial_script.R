8913f42ddc8e10e36a98f0e728c4c3b89bbc244d
Patricio Echeverria <patricio.echeverria@gmail.com>
2020-08-28 21:18:01 -0400

    actualizacion script y asssets

--- inicial_script.R
+++ inicial_script.R
@@ -5,3 +5,5 @@ setwd('~')
 dir1 <- paste(getwd(), "/DEVF/Curso Data Science/Proyecto/devF_proyecto/Assets", sep="")
 WD <- normalizePath(dir1, winslash = "\\", mustWork = NA)
 f416 <- read.csv(paste(WD, "Abr2019-Jun2019wet.csv", sep="\\"), stringsAsFactors = FALSE)
+
+str(f416)
