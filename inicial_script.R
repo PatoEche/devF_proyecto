badc875087f11cbf7a07e005f712b8261d1c6935
Patricio Echeverria <patricio.echeverria@gmail.com>
2020-09-02 18:26:12 -0400

    rename, delete whitespace

--- inicial_script.R
+++ inicial_script.R
@@ -10,13 +10,15 @@ file416 <- read.csv(paste(WD, "Abr2019-Jun2019wet.csv", sep="\\"), stringsAsFact
 library('tidyverse')
 library(dplyr) # More efficient DATA FRAME working
 library(reshape) # Rename column
-library(data.table) # Modify de NA more efficient
+library(data.table) # Modify NA more efficient
 
 ### First view
 str(file416)
 
-### Create a DataFrame just with necessary columns
-f416 <- select(file416, ï..Date2, Textbox70, Product1, Textbox44, Pump1, Cashier1, Name1, MOP11)
+### Create a DataFrame just with necessary columns and modify name
+f416 <- select(file416, ï..Date2, Textbox70, Product1,Textbox42, Textbox44, Pump1, Cashier1, Name1, MOP11)
+
+str(f416)
 
 ### Verificacion de Nulos
 sapply(f416, function(x) sum(is.na(x)))
@@ -25,24 +27,11 @@ sapply(f416, function(x) sum(is.na(x)))
 ### SECCION DE MANIPULACION DE DATOS ELIMINAR SUMAR BORRAR AGREGAR CAMBIAR ########
 #-----------------------------------------------------------------------------------#
 
-### Delete unnecessary column
-#f416 <- f416 %>%
-#  select(-MOP21) %>%
-# select(-MOP31) %>%
-
 ### Modificacion de nombres de columnas
-f416 <- rename(f416, c(date=ï..Date2))
-f416 <- rename(f416, c(hour=Textbox70))
-f416 <- rename(f416, c(product=Product1))
-f416 <- rename(f416, c(value=Textbox44))
-f416 <- rename(f416, c(pump=Pump1))
-f416 <- rename(f416, c(cod_cashier=Cashier1))
-f416 <- rename(f416, c(name_cashier=Name1))
-f416 <- rename(f416, c(media_pay=MOP11))
+name_head <- colnames(f416)
+new_name_head <- c('date', 'hour', 'product', 'qty', 'value', 'pump', 'id_cashier','name_cashier', 'pay')
+names(f416)[names(f416) == name_head] <- new_name_head
 
-
-### Sumar cantidad de datos de una columna
-sum(f416$name_cashier=="MANUEL MARIN")
 
 ### Modificar los valores eliminando o agregando caracteres
 # f416$name_cashier = gsub('"', '', f416$name_cashier)
@@ -58,44 +47,59 @@ for(i in 1:nrow(f416)) {
 str(f416)
 backupF416 <- data.frame(f416)
 
-### Modify value float
+### Modify value
 f416$value = gsub(',00', '', f416$value)
 f416$value = gsub('\\.', '', f416$value)
 
-### Modify char in number
-fil416 <- transform(f416, value = as.numeric(value), 
+### Modify char
+f416 <- transform(f416, value = as.numeric(value), 
           cod_cashier = as.numeric(cod_cashier))
 
 ### Replace NA to 0
-fi416 <- replace(fil416, is.na(fil416), 0)
+f416 <- replace(f416, is.na(f416), 0)
 
 
+str(f416)
+print(f416$value[1])
 
-str(fi416)
-print(fi416$value[1])
-### Data and hour must be checked at future
+### Modify date and hour
+f416$date <- as.Date(f416$date)
 
+### AGRUPACION Y ANALISIS ESTADISTICO -----------------------------------
 #------------------------------------------------------------------------#
-#------------------------------------------------------------------------#
-
-
-sapply(f416, function(x) sum(is.na(x)))
-
-colnames(f416)
-rowname(f416)
-f416[,2]
-class(f416[,2])
-is.numeric(f416[,2])
-table(f416[,2])
-table(f416[,3])
-table(f416[,3])
-
 
 #### Creamos un bucle que recorrera la dimension completa (n columnas) de nuestra dataframe
 #### imprimiendo el nombre de la columna y una tabal con la distribucion absoluta de c/column
-for(i in 1:dim(f416)[2]) {
-  print(colnames(f416)[i])
-  print(table(f416[,i]))
+for(i in 1:dim(fi416)[2]) {
+  print(colnames(fi416)[i])
+  print(table(fi416[,i]))
 }
 
+f416 %>% 
+  group_by(cod_cashier) %>%
+  summarise(n_transacciones=n(),
+            ) %>%
+  filter(n_transacciones > 0,
+         )
 
+fi416 %>% 
+  group_by(date) %>%
+  summarise(n_transacciones=n(),
+            mean = mean(value)
+  ) %>%
+  filter(date < '05/04/2019',
+  )
+
+f416 %>% 
+  group_by(media_pay) %>%
+  summarise(n_transacciones=n(),
+            ) %>%
+  filter(n_transacciones > 0,
+        )
+
+value_day_hour <- f416 %>%
+  group_by(date) %>%
+#  group_by(hour) %>%
+  summarise(sum(value))
+  
+  
